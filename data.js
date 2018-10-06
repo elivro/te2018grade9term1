9229cbce139a34d70daa036e5204d5249bd4b553
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-10-06 13:18:05 -0700

    Updated

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+let message = "Hello World!"; let sibilings = 1;
+let birthDay = 12; let pets = 0; let feeling = "Happy";
+let phoneType = "Samsung";
+let name = "Orvil";
+let isUnderAged = true;
+let isLiving = true;
+let yourName;
 
+console.log(message);
+console.log(sibilings);
+console.log(birthDay);
+console.log(pets);
+console.log(feeling);
+console.log();
+console.log(name);
+console.log(isUnderAged);
+console.log(isLiving);
+console.log (yourName);
 // Type your code below!
