4b6971f54bc760d902ab04c481882b8290b5a5dc
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-11-03 15:49:45 -0700

    items

--- haunted.js
+++ haunted.js
@@ -6,26 +6,41 @@ let player = {
   courage: 0,
   intellect: 0,
   strength: 0,
-  charm: 0,
   weapon: "fists",
-  item: "flashlight",
+
 };
 
 console.log();
-console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
+console.log("*** Breaking News! The United States has entered an international war! There is little information about how the war started, but we encourage people to stay home and remain calm! I repeat stay home and remain.... ***");
 player.name = READLINE.question("What is your name?: ");
-console.log(`${player.name} stands in front of a spooky haunted house.`);
-console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
-let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
-  player.courage++;
-  console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-  // continue the story
-
-} else {
+console.log("Your friends ask you what are you going to do?");
+let stayAlive = READLINE.question("Will you stay home? (yes or no) ");
+if(stayAlive == "y" || stayAlive == "yes") {
   player.intellect++;
-  console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
+  console.log("We should stay home. I'm sure the milita will come soon and evacuate us to somewhere safe.");
+  console.log("Your friends agree to stay home and wait.");
 
+} else(stayAlive == "n" || stayAlive == "no") {
+  player.courage++;
+  console.log("oh, I know a secret bunker that was used during world war 2. We could probably hide there for a while. Quick grab everything you can. We're leaving in 5.");
+  console.log("Everybody's in the car. You notice that various other people have left as well. You don't notice many cars on the street. You're neighboor sits outside on the his porch. He says the war started yesterday and most people left yesterday. ");
+  console.log(player.name+" hurry up we don't have time to waste.");
+  console.log("You get in the car, but before you go to the bunker you have a choice... ")
+  let decisonAfter = READLINE.question("Will you go to Costco and take what ever tools and food you find(1) or will you go to the gun shop and take what ever weapons you find?");
+    if (decisionAfter == 1) {
+    console.log("There is no one at Costco, but you did find food and tools.");
+    }
+  else if (decisonAfter == 2){
+    console.log("You tell everyone to wait inside the car. The door to the gun shop is closed. ")
+    console.log();
+    decisionAfter = READLINE.question("Will you break the door with a brick (1) or will you head back to the car(2)")
+    if(decisionAfter == 1){
+      player.strength++;
+      console.log("You pick up a shotgun, 2 night vision googles, and a semi automatic gun. " );
+      player.weapon2 = "shotgun";
+      player.item = "night vision googles"
+      console.log("you head back to the car and head towards the bunker.")
+    }
+  }
 }
 console.log("Thanks for playing!");
